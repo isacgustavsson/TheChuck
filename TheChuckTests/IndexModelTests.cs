96ba012608aa942960f58a1568512fd72684a32d
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-06-15 13:03:41 +0200

    build: test deployment during tests

--- TheChuckTests/IndexModelTests.cs
+++ TheChuckTests/IndexModelTests.cs
@@ -12,7 +12,7 @@ namespace TheChuck.Pages.Tests
         public async Task OnGet_ShouldDisplayTextFromService()
         {
             //Arrange
-            var joke = new Joke() { Value = "Works"};
+            var joke = new Joke() { Value = "Works" };
             var sut = new IndexModel(NullLogger<IndexModel>.Instance, new JokeServiceFake(joke));
 
             //Act
@@ -35,21 +35,21 @@ namespace TheChuck.Pages.Tests
             Assert.AreEqual("Något gick fel. Försök igen lite senare.".ToUpper(), sut.DisplayText.ToUpper());
         }
 
-        
+
         [TestMethod()]
         public async Task OnGet_ShouldBeUppecase()
         {
             //Arrange
-            var joke = new Joke() { Value = "Works"};
+            var joke = new Joke() { Value = "Works" };
             var pageModel = new IndexModel(NullLogger<IndexModel>.Instance, new JokeServiceFake(joke));
 
             //Act
             await pageModel.OnGet();
 
             //Assert
-            Assert.AreEqual("WORKS", pageModel.DisplayText);
+            Assert.AreEqual("NotWorks", pageModel.DisplayText);
         }
-        
+
 
     }
 }
